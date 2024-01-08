2bd0b7087ff1d167d5df2ae94b713f74cb216a32
Lov Kumar <119415006+lovkumar120169@users.noreply.github.com>
2024-01-08 23:50:22 +0530

    Time: 65 ms (23.90%), Space: 42.8 MB (33.53%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack=[]
+    for(let i=0;i<s.length;i++){
+        if(stack.length==0){
+            stack.push(s[i])
+        }else if((stack[stack.length-1]=="(" && s[i]==")") || (stack[stack.length-1]=="{" && s[i]=="}") || (stack[stack.length-1]=="[" && s[i]=="]")){
+            stack.pop()
+        }else{
+            stack.push(s[i])
+        }
+    }
+    if(stack.length==0){
+        return true
+    }else{
+        return false
+    }
+};
